46389b97f5ec8255c347a61bf4ccd6977839bdd3
jocimar_neres <60933225+jocimar-dev@users.noreply.github.com>
2021-03-10 09:17:41 -0300

    Finaliza módulos de 2 e 3 de orientação a objetos

--- src/main.kt
+++ src/main.kt
@@ -1,20 +1,101 @@
-import javax.sql.rowset.JoinRowSet
+fun main() {
+    println("Bem vindo ao Bytebank")
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
 
-fun main (args: Array<String>){
-    println("Bem vindo ao Banco Zup!!")
-    val numeroDaConta: Int = 1
-    val titular: String = "José"
-    var saldo: Double = 0.0
-    saldo = 100.00
-    saldo = saldo + 100
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
 
-    println("titular $titular")
-    println("número da Conta $numeroDaConta")
-    println("saldo $saldo")
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
 
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    println("depositando na conta do Alex")
+    deposita(contaAlex, 50.0)
+    println(contaAlex.saldo)
+
+    println("depositando na conta da Fran")
+    deposita(contaFran, 70.0)
+    println(contaFran.saldo)
 }
-//Resultado:
-//Bem vindo ao Banco Zup!!
-//titular José
-//número da Conta 1
-//saldo 200.0
+
+fun deposita(conta: Conta, valor: Double){
+    conta.saldo += valor
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaCopiasEReferencias(){
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+}
+
+fun testaLacos(){
+    var i = 0
+    while(i < 5){
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
+
+    for (i in 5 downTo 1) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+    }
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
+}
